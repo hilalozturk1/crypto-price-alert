7b7b7be4460db40dee91145e83b4cc3e10d295f3
hilalozturk1 <hlloztrk68@gmail.com>
2025-06-19 01:11:16 +0300

    error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { connectMongoDB } from "./config/db";
 import alertRoutes from "./routes/alertRoutes";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger";
+import { errorHandler } from "./utils/errorHandlers";
 
 const app = express();
 const router = Router();
@@ -17,6 +18,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/api", router);
 app.use("/api/alerts", alertRoutes);
+
+app.use(errorHandler);
 
 connectMongoDB().then(() => {
   app.listen(3000, () => {
