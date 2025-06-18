b21b31dc028d7348393742d99e5f83d9096fbddd
hilalozturk1 <hlloztrk68@gmail.com>
2025-06-19 00:09:31 +0300

    swagger config

---
+++ src/config/swagger.ts
@@ -0,0 +1,35 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Crypto Price Alert Service API",
+      version: "1.0.0",
+      description: "cryptocurrency price alerts.",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/api",
+        description: "Local Development Server",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts", "./src/models/*.ts"],
+};
+
+export const swaggerSpec = swaggerJsdoc(swaggerOptions);
