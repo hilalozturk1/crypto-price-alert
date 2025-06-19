1b315a725bac4d2fce234d72a59f17ef908f644c
hilalozturk1 <hlloztrk68@gmail.com>
2025-06-19 19:37:59 +0300

    redis connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,26 @@
 import mongoose from "mongoose";
+import Redis from 'ioredis';
+
+import { config } from './index';
 
 export const connectMongoDB = async () => {
   try {
     const dbUri =
       process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/cryptoalerts";
     await mongoose.connect(dbUri, {});
+    console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1);
   }
 };
+
+export const redisClient = new Redis(config.redisUrl);
+
+redisClient.on('connect', () => {
+  console.log('Redis connected successfully');
+});
+
+redisClient.on('error', (err) => {
+  console.error('Redis connection error:', err);
+});
