856bf811cdf3eaa8269a89246389398b87b08df8
hilalozturk1 <hlloztrk68@gmail.com>
2025-06-19 20:31:54 +0300

    feat: Implement Redis Pub/Sub for alert notifications

--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -1,20 +1,60 @@
 import { AlertDocument } from "../models/alert";
-class NotificationService {
 
-  public async sendNotification(alert: AlertDocument, currentPrice: number): Promise<void> {
+import { redisClient } from "../config/db";
+import { REDIS_CHANNELS } from "../config/redisChannels";
+
+class NotificationService {
+  private subscriber: typeof redisClient;
+
+  constructor() {
+    this.subscriber = redisClient.duplicate();
+    this.setupSubscriber();
+  }
+
+  private setupSubscriber() {
+    this.subscriber.on("message", (channel, message) => {
+      if (channel === REDIS_CHANNELS.ALERT_TRIGGERED) {
+        try {
+          const { alert, currentPrice } = JSON.parse(message);
+          this.sendNotification(alert, currentPrice);
+        } catch (error) {
+          console.error("Error processing message:", error);
+        }
+      }
+    });
+
+    this.subscriber.subscribe(REDIS_CHANNELS.ALERT_TRIGGERED, (err, count) => {
+      if (err) {
+        console.error(
+          `Failed to subscribe to channel: ${REDIS_CHANNELS.ALERT_TRIGGERED}`,
+          err,
+        );
+      } else {
+        console.log(
+          `Subscribed to channel: ${REDIS_CHANNELS.ALERT_TRIGGERED}. Total subscriptions: ${count}`,
+        );
+      }
+    });
+  }
+  private async sendNotification(
+    alert: AlertDocument,
+    currentPrice: number,
+  ): Promise<void> {
     const message = `
       Alert!
       Crypto: ${alert.symbol.toUpperCase()}
       Target Price: $${alert.targetPrice}
       Current Price: $${currentPrice}
-      Alert Type: ${alert.alertType === 'above' ? 'Above' : 'Below'}
+      Alert Type: ${alert.alertType === "above" ? "Above" : "Below"}
       UserId: ${alert.userId}
       Alert ID: ${alert._id}
     `;
 
     console.log(`*********Notification sent: ${message}`);
   }
-
+  public init() {
+    console.log("Notification service initialized and listening for alerts.");
+  }
 }
 
 export const notificationService = new NotificationService();
